fa53b12008fbfecfb21646537100b81ed82f95b2
andykong18 <35146432+andykong18@users.noreply.github.com>
2018-02-22 23:26:39 -0800

    Update RideType.sql

--- RideType.sql
+++ RideType.sql
@@ -1,1 +1,9 @@
-INSERT INTO RideType (RideTypeID,TypeDescription) VALUES (1,"UberX"),(2,"Pool"),(3,"SELECT"),(4,"UberXL"),(5,"Black"),(6,"SUV"),(7,"WAV");
+INSERT INTO RideType (RideTypeID,TypeDescription) 
+VALUES 
+(1,"UberX"),
+(2,"Pool"),
+(3,"SELECT"),
+(4,"UberXL"),
+(5,"Black"),
+(6,"SUV"),
+(7,"WAV");
