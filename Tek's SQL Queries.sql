67fbe7128dbf3f911c649f7f71424bad0b248ef6
tekchheang <t.chheang.k@gmail.com>
2018-02-22 21:51:37 -0800

    Queries

--- Tek's SQL Queries.sql
+++ Tek's SQL Queries.sql
@@ -9,3 +9,24 @@ WHERE
     d.driverid = dl.driverid
         AND u.username = d.username
         AND DATEDIFF(expirationDate, CURDATE()) <= 89;
+
+#Count Number of Female Driver (Please test this)
+SELECT
+    COUNT(*)
+FROM
+    driverlicense
+WHERE
+    driverlicense.gender = "Female";
+    
+#Subtotal For CurrentDate (Please test this)
+SELECT
+    SUM(f.amount) AS FareGrandTotal,
+    SUM(f.basefare) AS BaseFeeGrandTotal,
+    SUM(f.surge) AS SurgeGrandTotal,
+    SUM(f.tolls) AS TollGrandTotal,
+FROM
+    Fare f,
+    receipt r
+WHERE
+    f.receiptNumb = r.receiptNumber
+        AND DATEDIFF(r.receiptDate, CURDATE()) = 0;
