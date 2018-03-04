1bcd761df5cf166c884767f84565333a2f3ef942
tammk <36558160+tammk@users.noreply.github.com>
2018-03-04 15:49:13 -0800

    Update vehicletype.sql

--- vehicletype.sql
+++ vehicletype.sql
@@ -1,6 +1,5 @@
-﻿INSERT INTO vehicletype (VehicleTypeID,typeDescription,engineType) 
-VALUES 
-(1,"Sedan","Regular"),
+INSERT INTO vehicletype (VehicleTypeID,typeDescription,engineType) 
+VALUES (1,"Sedan","Regular"),
 (2,"Luxury","Hybrid"),
 (3,"SUV","Hybrid"),
 (4,"Minivan","Regular"),
